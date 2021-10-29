91f0849b739948608c90ae176a6ea09d28ea1b67
derekzx <chinzhixian@gmail.com>
2021-10-29 16:11:16 -0700

    Add limit sell option

--- src/databaseClient.js
+++ src/databaseClient.js
@@ -13,10 +13,21 @@ const tokenPricePool = mysql.createPool({
   queueLimit: 5,
 }).promise();
 
-const limitOrderPool = mysql.createPool({
+const limitBuyPool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_LIMIT_ORDER,
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`, // For production
+  // host: `${process.env.DB_HOST}`, // For local testing
+  connectionLimit: 20,
+  waitForConnections: true,
+  queueLimit: 5,
+}).promise();
+
+const limitSellPool = mysql.createPool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_LIMIT_SELL,
   socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`, // For production
   // host: `${process.env.DB_HOST}`, // For local testing
   connectionLimit: 20,
@@ -35,4 +46,4 @@ const stopLossPool = mysql.createPool({
   queueLimit: 5,
 }).promise();
 
-module.exports = {tokenPricePool, limitOrderPool, stopLossPool};
+module.exports = {tokenPricePool, limitOrderPool, limitSellPool, stopLossPool};
